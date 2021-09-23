e4c03cc20c1be4cc0ae1265edd3166191a831485
DDDHuang <904897578@qq.com>
2021-09-23 14:50:55 +0800

    feat: add out put

--- src/minirest_handler.erl
+++ src/minirest_handler.erl
@@ -82,6 +82,7 @@ dispatch(Req, Route, Filter) ->
     end.
 
 dispatch(Req, #{module := Mod, func := Fun, bindings := Bindings}) ->
+    io:format("~p~n", [Req]),
     case catch parse_params(Req) of
         {'EXIT', Reason} ->
             error_logger:error_msg("Params error: ~p", [Reason]),
